f8266b76c081422b1b6af7ec4999574aa729b9fa
fduarte896 <66535395+fduarte896@users.noreply.github.com>
2025-04-09 10:33:18 -0500

    Initial Commit

---
+++ Drunken Cards/Drunken_CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Drunken_CardsApp.swift
+//  Drunken Cards
+//
+//  Created by Felipe Duarte on 9/04/25.
+//
+
+import SwiftUI
+
+@main
+struct Drunken_CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
