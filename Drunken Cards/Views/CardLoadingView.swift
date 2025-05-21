ee057ee133eb86889a630772dd7ecc3a62a290ae
fduarte896 <66535395+fduarte896@users.noreply.github.com>
2025-05-21 10:17:55 -0500

    First commit, work in progress!

---
+++ Drunken Cards/Views/CardLoadingView.swift
@@ -0,0 +1,18 @@
+//
+//  CardLoadingView.swift
+//  Drunken Cards
+//
+//  Created by Felipe Duarte on 7/05/25.
+//
+
+import SwiftUI
+
+struct CardLoadingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardLoadingView()
+}
